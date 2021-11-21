5d0ba0e9d19cc865a822c14c5c62794338559bda
David Reyes Santoyo <deiv@Davids-MacBook-Pro.local>
2021-11-20 18:46:39 -0800

    Added variables and character limit for password

---
+++ Develop/script.js
@@ -0,0 +1,30 @@
+// Assignment code here
+
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numerics = "0123456789";
+var spchar = "#$%&'()*+,-./:;<=>?@[]/^_`{|}~";
+
+
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+// switch 
